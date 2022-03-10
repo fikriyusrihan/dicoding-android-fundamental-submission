13dc63cbfa570fb6bf52983f972a3e463471be3b
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-03-10 12:12:16 +0700

    implement Dagger in FavoriteActivity.kt

--- app/src/main/java/com/artworkspace/github/ui/viewmodel/FavoriteViewModel.kt
+++ app/src/main/java/com/artworkspace/github/ui/viewmodel/FavoriteViewModel.kt
@@ -4,12 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.artworkspace.github.data.UserRepository
 import com.artworkspace.github.data.local.entity.UserEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FavoriteViewModel(private val repository: UserRepository) : ViewModel() {
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(private val repository: UserRepository) : ViewModel() {
 
     private val _favorites = MutableStateFlow(listOf<UserEntity>())
     val favorite = _favorites.asStateFlow()
